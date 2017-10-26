5aa48946fd14d9e31930e4166d0fc59914b959f9
Jonathan "Duke" Leto <jonathan@leto.net>
2017-10-25 20:27:25 -0700

    Add some POD

---
+++ lib/Alien/Bitcoin.pm
@@ -0,0 +1,54 @@
+package Alien::Bitcoin;
+use strict;
+use warnings;
+use base qw( Alien::Base );
+our $VERSION = '0.01';
+
+=head1 NAME
+
+Alien::Bitcoin - Easily install Bitcoin full node
+
+=head1 DESCRIPTION
+
+This allows you to tell your favorite CPAN installer to install Bitcoin, via
+your local CPAN mirror. This allows Perl applications to specify that they
+depend on an external dependency, Bitcoin, in a standard way, the Alien::*
+namespace. Most likely you will not use this module directly, you just want
+to do
+
+    cpan Alien::Bitcoin
+
+to install with the normal CPAN.pm client or:
+
+    cpanm Alien::Bitcoin
+
+to use the newer, more featureful cpanminus client.
+
+=head1 AUTHOR
+
+Duke Leto <duke@leto.net>
+
+=head1 SUPPORT THIS WORK
+
+Send your BTC donations to
+
+1GNZmtxMtha55Ph4c3k3cZ2xbST86naTmr
+
+to support this CPAN module. Thanks!
+
+=head1 SEE ALSO
+
+Alien::Hush, Alien::Zcash, The Future
+
+=head1 COPYRIGHT
+
+Copyright (c) 2017 by Duke Leto <duke@leto.net>.  All rights reserved.
+
+=head1 LICENSE AGREEMENT
+
+This package is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself, Fuck Yeah!
+
+=cut
+
+"Satoshi's Revenge"+0.0 ? 0e0 : 0e0;
